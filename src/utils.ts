031325696f6e162861e34a82591b7600f2c16204
Paul Vudmaska <paul.vudmaska@gmail.com>
2023-06-26 11:07:07 -0500

    rename yin att utils fix for . during deploy

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 export function onlyFileName(s: String) {
   s = s.substring(s.lastIndexOf("/") + 1);
   s = s.replace(/\.webp/, "");
+  let sdot = s.indexOf('.')
+  if(sdot != -1) s = s.substring(0, sdot)
   return s;
 }
